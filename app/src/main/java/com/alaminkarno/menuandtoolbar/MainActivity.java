b40bf78dc620f230bff66df3d6dac68476de165f
User <https://github.com/karno786/Sqlite_git_test>
2020-12-05 00:03:29 +0600

    13. Toolbar - Android Development Course Basic To Advanced

--- app/src/main/java/com/alaminkarno/menuandtoolbar/MainActivity.java
+++ app/src/main/java/com/alaminkarno/menuandtoolbar/MainActivity.java
@@ -2,19 +2,27 @@ package com.alaminkarno.menuandtoolbar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setTitle("Home");
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
     }
 
@@ -40,5 +48,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void button(View view) {
+        startActivity(new Intent(MainActivity.this,MainActivity2.class));
     }
 }
